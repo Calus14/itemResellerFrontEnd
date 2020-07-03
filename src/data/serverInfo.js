2e5d055cff6174aaab10b60ceec3a2a9495f1b7c
chblaze@gmail.com <candjblaze@gmail.com>
2020-07-02 22:16:47 -0500

    added secrets to heroku instead of hard coded

--- src/data/serverInfo.js
+++ src/data/serverInfo.js
@@ -1,6 +1,6 @@
 export default{
-  serverUrl: "http://localHost",
-  serverPort: 8801,
+  serverUrl: process.env.SERVER_URL ||  "http://localHost",
+  serverPort: process.env.SERVER_PORT || 8801,
 
   //ENDPOINTS
   basicSearchEndpoint: "listOfItems",
